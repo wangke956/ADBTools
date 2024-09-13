3482584c29d5a5e0d8430b3f4a4c18c503650ffa
userName <userEmail>
2024-09-13 19:40:31 +0800

    提交初始代码，版本7.0，可用

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from ADB_module import ADB_Mainwindow
+import qdarkstyle
+
+
+
+def main():
+    app = QApplication(sys.argv)
+    # app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='pyqt5'))
+    window = ADB_Mainwindow()  # 创建窗口
+    window.show()  # 显示窗口
+    sys.exit(app.exec())  # 退出时关闭窗口
+if __name__ == '__main__':  # 主函数
+    main()
