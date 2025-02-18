b55d02eff2353e115f2ce3af5a20d25af3170c43
wangke <wkwk321@hotmail.com>
2025-02-18 17:24:15 +0800

    0218

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from ADB_module import ADB_Mainwindow
-import qdarkstyle
+# import qdarkstyle
 """功能完整，已测试"""
 
 def main():
     app = QApplication(sys.argv)
-    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    # app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
     # app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='pyqt5'))
     window = ADB_Mainwindow()  # 创建窗口
     window.show()  # 显示窗口
