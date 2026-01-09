060320dc44668f7bea3b7ca75947c5c42fd0b5a4
wangke <wkwk321@hotmail.com>
2026-01-09 12:48:35 +0800

    1.更新版本号V1.4

--- ADB_module.py
+++ ADB_module.py
@@ -36,7 +36,7 @@ class TextEditOutputStream(io.TextIOBase):  # 继承 io.TextIOBase 类
 # noinspection DuplicatedCode,SpellCheckingInspection
 class ADB_Mainwindow(QMainWindow):
     # 软件版本常量
-    VERSION = "1.3"
+    VERSION = "1.4"
     
     def __init__(self, parent=None):
         super(ADB_Mainwindow, self).__init__(parent)
